c76344b1ac6161368a33450cb969df0e0afd7396
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-01-14 11:26:41 +0530

    Use rails named URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help', as: '/helf'
+  # get 'static_pages/about', to: 'static_pages#about', as: '/about'
+  get '/aboux', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
