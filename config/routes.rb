4a33ff57ca1112d04a14473b2149fc827e579a4d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-01 22:45:10 +0530

    Add user resource route

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
